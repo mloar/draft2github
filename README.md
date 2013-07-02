deca39b2d47402c574524489231a0d98888c5d96
Matt Loar <matt@loar.name>
2013-07-02 08:36:14 +0000

    Initial commit of draft2github.

---
+++ README.md
@@ -0,0 +1,43 @@
+# draft2github
+
+A webhook for publishing to [GitHub Pages](http://pages.github.com/) from [Draft](https://draftin.com).
+
+## Getting Started
+
+draft2github can be installed via [npm](https://github.com/isaacs/npm).
+
+    npm install draft2github
+
+Alternatively, clone the git repository.
+
+    git clone git://github.com/mloar/draft2github.git
+
+## Configuration
+
+See `settings.json`.
+
+## Supporting Modules
+
+* [Bogart](https://github.com/nrstott/bogart): `npm install bogart`
+* [Github3](https://github.com/edwardhotchkiss/github3): `npm install github3`
+
+## License
+
+Copyright (c) 2009 Matt Loar <[matt.loar.name](http://matt.loar.name/)\>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.js
@@ -0,0 +1,68 @@
+var bogart  = require('bogart');
+
+var getPostPath = function (draft_data) {
+    return new Date(draft_data.created_at).toISOString().substr(0, 10) + '-' + draft_data.name.replace(/ /g,
+    '-').replace(/[\/\\%&?]/g, '').toLowerCase();
+};
+
+var getContentForPost = function (draft_data) {
+    return "--- \n" +
+        "layout: post\n" +
+        "title: " + draft_data.name + "\n" +
+        "date: " + draft_data.updated_at +"\n" +
+        "---\n\n" +
+        draft_data.content;
+};
+
+var newGitHubPagesPost = function (draft_data) {
+    var settings = require('./settings.json');
+    var gh     = require('github3');
+    gh.setAccessToken(settings.access_token);
+    var getRef = bogart.promisify(gh.getLastCommitRef, gh);
+    var getTree= bogart.promisify(gh.getTree, gh);
+    var createTreeAndAddFile= bogart.promisify(gh.createTreeAndAddFile, gh);
+    var updateRefHead= bogart.promisify(gh.updateRefHead, gh);
+    var createCommit= bogart.promisify(gh.createCommit, gh);
+    var last_commit;
+
+    return getRef(settings.repo_name, settings.user_name, settings.branch).then(function (ref) {
+        last_commit = ref.object.sha;
+        return getTree(settings.repo_name, settings.user_name, ref.object.sha);
+    }).then(function (oldTree) {
+        var post_date = new Date(draft_data.created_at);
+        return createTreeAndAddFile(settings.repo_name, settings.user_name, '_posts/' + getPostPath(draft_data) + '.markdown',
+        getContentForPost(draft_data), oldTree.sha).then(function (newTree) {
+            return createCommit(settings.repo_name, settings.user_name, "New post from Draft", newTree.sha, last_commit,
+                {'email': draft_data.user.email, 'date': new Date().toISOString()});
+        });
+    }).then(function (commit) {
+        return updateRefHead(settings.repo_name, settings.user_name, settings.branch, commit.sha, false);
+    }).then(function (result) {
+        var str = JSON.stringify(result);
+        return {
+            status: 200,
+            body: [str],
+            headers: {
+                "Location": "http://" + settings.repo_name + "/" + getPostPath(draft_data),
+                "Content-Type": "application/json",
+                "Content-Length": Buffer.byteLength(str, "utf-8")
+            }
+        };
+    });
+};
+
+var router     = bogart.router();
+router.post('/', function (req) {
+    return newGitHubPagesPost(JSON.parse(req.body.payload));
+});
+
+var app = bogart.app();
+
+// Include batteries, a default JSGI stack.
+app.use(bogart.batteries);
+
+// Include our router, it is significant that this is included after batteries.
+app.use(router);
+
+var port = process.env.PORT || 5000;
+app.start(port);
